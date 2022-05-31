b5e62de0a899c636656a6a0a52a3674cf12f9606
ximena9201 <ximenandrea.ro@gmail.com>
2022-05-31 18:20:02 -0400

    fix typo

--- scripts/nuke.sh
+++ scripts/nuke.sh
@@ -42,6 +42,6 @@ export AWS_REGION=us-east-1
   $HOME/bin/aws-nuke --access-key-id "$AWS_ACCESS_KEY_ID" \
     --secret-access-key "$AWS_SECRET_ACCESS_KEY" \
     --session-token "$AWS_SESSION_TOKEN" \
-    --config nuke-config.yml \
+    --config nuke-config.yaml \
     --force \
     --no-dry-run || exit_code=$?
